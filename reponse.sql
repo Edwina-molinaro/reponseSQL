98505a8d81225891a63ccedb2f4b5b57d87dd0bc
Edwina-molinaro <61154469+Edwina-molinaro@users.noreply.github.com>
2020-04-02 17:59:17 +0200

    exo2

--- reponse.sql
+++ reponse.sql
@@ -4,3 +4,12 @@ WHERE category_id = idCat
 AND products.brand_id = brands.brand_id
 AND brand_name = 'Haro'
 AND nameCat = 'Electric Bikes'
+
+----
+
+SELECT name, model_year, price,((price*20)/100) AS TVA,(price+((price*20)/100)) AS TTC,nameCat, brand_name
+FROM products, brands,categories
+WHERE category_id = idCat
+AND products.brand_id = brands.brand_id
+AND brand_name = 'Trek'
+AND nameCat = 'Electric Bikes'
