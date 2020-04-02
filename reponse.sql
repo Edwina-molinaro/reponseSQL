9fa793ed51a36b6c77e24865c8288f522f05f3f8
Edwina-molinaro <61154469+Edwina-molinaro@users.noreply.github.com>
2020-04-02 15:50:04 +0200

    correction

--- reponse.sql
+++ reponse.sql
@@ -1,3 +1,4 @@
 SELECT product_name, model_year, list_price, category_id, brand_name 
-FROM Electric Bikes
+FROM product_id, category_id, category_name, brand_id, brand_id
 WHERE brand_name = Haro
+AND category_id = Electric_Bikes 
